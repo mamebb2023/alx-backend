aeadbbf6bf5ba86f926515b84ca2fdbc72d6eb66
Mohammednur <mamebb2023@gmail.com>
2024-01-09 14:33:15 +0300

    Flask App 1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" Flask API
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+class Config:
+    """ Config class
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFUALT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/")
+def index():
+    """ route to home page (index.html)
+    """
+    return render_template("0-index.html")
