6d940de586b6487293b0ab96e607b4edee4b8e3a
Mohammednur <mamebb2023@gmail.com>
2024-01-10 12:16:34 +0300

    Flask App 1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python3
-""" Flask API
-"""
+'''Task 0: Basic Flask app
+'''
+
 from flask import Flask, render_template
 from flask_babel import Babel
 
 
 class Config:
-    """ Config class
-    """
+    '''Config class'''
+
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFUALT_TIMEZONE = "UTC"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
+
 babel = Babel(app)
 
 
-@app.route("/")
+@app.route('/')
 def index():
-    """ Route to home page (index.html)
-    """
-    return render_template("1-index.html")
+    '''default route'''
+    return render_template("1-index.html",)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
