3242405561a056c650ea6192471b6ee9feb4969f
Mohammednur <mamebb2023@gmail.com>
2024-03-06 13:07:30 +0300

    Hypermedia Del Pagination

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
-"""
-Deletion-resilient hypermedia pagination
+""" Deletion-resilient hypermedia pagination
 """
 
 import csv
 import math
-from typing import List
+from typing import Dict, List, Optional
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
+    """ Server class to paginate a database of popular baby names.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
@@ -18,7 +17,7 @@ class Server:
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
+        """ Cached dataset
         """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
@@ -29,7 +28,7 @@ class Server:
         return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
-        """Dataset indexed by sorting position, starting at 0
+        """ Dataset indexed by sorting position, starting at 0
         """
         if self.__indexed_dataset is None:
             dataset = self.dataset()
@@ -40,25 +39,27 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-        """ Returns with the specified key value paires """
-        data = self.indexed_dataset()
-        assert index is not None and index >= 0 and index <= max(data.keys())
-        page_data = []
-        data_count = 0
-        next_index = None
-        start = index if index else 0
-        for i, item in data.items():
-            if i >= start and data_count < page_size:
-                page_data.append(item)
-                data_count += 1
-                continue
-            if data_count == page_size:
-                next_index = i
-                break
-        page_info = {
-            'index': index,
-            'next_index': next_index,
-            'page_size': len(page_data),
-            'data': page_data,
-        }
-        return page_info
+        """ Returns page data plus info to next and prev pages
+            - Args:
+                - page: page to look for information
+                - page_size: size of each page
+            - Returns: dict
+        """
+        indexed_data = self.indexed_dataset()
+        indexed_data_length = len(indexed_data)
+        assert index is not None and index < indexed_data_length and index >= 0
+        truncated_data: List[List] = []
+        next_index = index
+        while len(truncated_data) < page_size and \
+                next_index < indexed_data_length:
+            data = indexed_data.get(next_index)
+            if data:
+                truncated_data.append(data)
+            next_index += 1
+
+        page_size = len(truncated_data)
+        response_data = {'index': index,
+                         'next_index': next_index,
+                         'page_size': page_size,
+                         'data': truncated_data}
+        return response_data
